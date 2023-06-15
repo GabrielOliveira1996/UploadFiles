e37ff79ff06443e8c4fbc7794d47eeafac1a49e2
Gabriel Oliveira <gab-oliveira96@hotmail.com>
2023-06-15 11:15:13 -0300

    style: :art: Improved design of file upload page, added card and lines for better layout organization.

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -4,9 +4,35 @@
 
 <div class="container d-flex justify-content-center">
 
-    <div class="row mt-5">
-        <input type="file" class="form-control">
+<div class="card col-md-6 m-5">
+    <div class="card-body">
+        <div class="row mt-5">
+
+            <div id="app">
+                <div class="row d-flex justify-content-center p-3">
+                    <div class="border p-5 d-flex justify-content-center">
+                        <img src="{{ asset('img/icons/envio.png') }}" class="col-md-3">
+                    </div>
+                </div>
+                <div class="row mx-1 mt-2">
+                    <label>Nome: </label>
+                    <label>Tipo: </label>
+                    <label>Tamanho: </label>
+                </div>
+                <div class="row d-flex justify-content-center">
+                    <div class="col-md-12">
+                        <input type="file" class="form-control mt-2">
+                    </div>
+                    <div class="col-md-12">
+                    <button type="submit" class="btn btn-primary mt-2 col-md-12">Carregar</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
+</div>
+    
 
 </div>
 
