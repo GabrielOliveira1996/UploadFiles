1e166575f67dabef86bbcdc7ac7a48cc58fddd7b
Gabriel Oliveira <gab-oliveira96@hotmail.com>
2023-06-15 12:22:06 -0300

    feat: Developed base for file upload, need to add exceptions for file types.

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -3,10 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class UploadController extends Controller
 {
+    private $_request;
+
+    public function __construct(Request $request){
+        $this->_request = $request;
+    }
+
     public function upload(){
-        return 'upload';
+        if($this->_request->hasFile('file')){
+            $file = $this->_request->file('file');
+            $path = $file->store('uploads');
+            return response()->json([
+                'message' => 'Arquivo enviado com sucesso.',
+                'path' => $path
+            ], 200);
+        }
+        return response()->json([
+            'message' => 'Nenhum arquivo enviado.'
+        ], 400);
     }
 }
