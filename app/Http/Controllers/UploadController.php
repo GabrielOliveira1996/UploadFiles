d0d50a91ab30c1b5d4d95aee3e104559a3c7376b
Gabriel Oliveira <gab-oliveira96@hotmail.com>
2023-06-17 21:35:54 -0300

    feat: Adjusted upload function for improvements.
    Captured file size and format. Added exceptions for file format and size.

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Exception;
 
 class UploadController extends Controller
 {
@@ -14,16 +16,29 @@ class UploadController extends Controller
     }
 
     public function upload(){
-        if($this->_request->hasFile('file')){
-            $file = $this->_request->file('file');
-            $path = $file->store('uploads');
+        try {
+            if($this->_request->hasFile('file')){
+                $file = $this->_request->file('file');
+                $size = $file->getSize(); 
+                $type = $file->getMimeType(); 
+                if($type == 'video/mp4' || $type == 'image/jpeg' || $type == 'image/jpg' || $type == 'image/png'){
+                    if($size > 104857600.04){
+                        throw new Exception('Não é possível enviar arquivo. O tamanho do arquivo excede o limite de 100 mb.', 413);
+                    }
+                    $path = $file->store('uploads');
+                    return response()->json([
+                        'message' => 'Arquivo enviado com sucesso.',
+                        'path' => $path
+                    ], 200);
+                }else{
+                    throw new Exception('Apenas vídeos e imagens podem ser enviados. São suportados apenas formatos mp4, png, jpg e jpeg.', 415);
+                }
+            }
+        }catch(Exception $e){
             return response()->json([
-                'message' => 'Arquivo enviado com sucesso.',
-                'path' => $path
-            ], 200);
-        }
-        return response()->json([
-            'message' => 'Nenhum arquivo enviado.'
-        ], 400);
+                'code' => $e->getCode(),
+                'message' => $e->getMessage(),
+            ], 400);
+        } 
     }
 }
